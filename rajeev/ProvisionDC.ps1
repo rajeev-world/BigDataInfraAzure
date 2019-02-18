fb0bd1b873a1c22f28179db0a2cc5f4aec71a779
rajeev-world <47442876+rajeev-world@users.noreply.github.com>
2019-02-18 16:27:29 +0530

    Add files via upload

--- rajeev/ProvisionDC.ps1
+++ rajeev/ProvisionDC.ps1
@@ -2,25 +2,26 @@ Configuration MyWebServerCfg2 {
  
     Import-DscResource -ModuleName PsDesiredStateConfiguration
  
-    Node 'localhost' {
+   node ("localhost") {
  
         # The first resource block ensures that the Web-Server (IIS) feature is enabled.
-        WindowsFeature IIS {
+       # The second resource block ensures that the Web-Mgmt-Tools feature is enabled.
+        
+	WindowsFeature IIS-Tools {
+            Ensure    = "Absent"
+            Name      = "Web-Mgmt-Tools"
+            DependsOn = "[WindowsFeature]IIS"
+        }   
+	WindowsFeature IIS {
             Ensure = "Absent"
             Name   = "Web-Server"
         }
- 
-        # The second resource block ensures that the Web-Mgmt-Tools feature is enabled.
-        WindowsFeature IIS-Tools {
-            Ensure    = "Present"
-            Name      = "Web-Mgmt-Tools"
-            DependsOn = "[WindowsFeature]IIS"
-        }
-         File WebsiteContent {
+ 	    File ApacheDownload {
             Ensure = 'Present'
             SourcePath = 'https://home.apache.org/~steffenal/VC15/binaries/httpd-2.4.38-win64-VC15.zip'
             DestinationPath = 'c:\'
         }
+ 
  
     }
 }
