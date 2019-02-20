4eddbfc81f0184bd45b4172c04e642fe4d8f5a2a
rajeev-world <47442876+rajeev-world@users.noreply.github.com>
2019-02-20 14:42:59 +0530

    Update myscript2.sh

--- rajeev/myscript2.sh
+++ rajeev/myscript2.sh
@@ -1,7 +1,7 @@
 # Installing OMI:
-wget https://github.com/Microsoft/omi/releases/download/v1.6.0/omi-1.6.0-0.ssl_100.ulinux.x64.rpm 
-sudo rpm -Uvh ./omi-1.6.0-0.ssl_100.ulinux.x64.rpm
+sudo wget -P /tmp/ https://github.com/Microsoft/omi/releases/download/v1.6.0/omi-1.6.0-0.ssl_100.ulinux.x64.rpm 
+sudo rpm -Uvh /tmp/omi-1.6.0-0.ssl_100.ulinux.x64.rpm
 
 # Installing DSC:
-wget  https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_098.x64.rpm
-sudo rpm -Uvh ./dsc-1.1.1-294.ssl_098.x64.rpm
+sudo wget -P /tmp/  https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_098.x64.rpm
+sudo rpm -Uvh /tmp/dsc-1.1.1-294.ssl_098.x64.rpm
