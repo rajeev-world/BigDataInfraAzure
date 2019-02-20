8e74da3d4b4ef36cce18574d87cec09e13664e00
rajeev-world <47442876+rajeev-world@users.noreply.github.com>
2019-02-20 14:44:36 +0530

    Create myscript1.sh

---
+++ rajeev/myscript1.sh
@@ -0,0 +1,7 @@
+# Installing OMI:
+sudo wget -P /tmp/ https://github.com/Microsoft/omi/releases/download/v1.6.0/omi-1.6.0-0.ssl_100.ulinux.x64.rpm 
+sudo rpm -Uvh /tmp/omi-1.6.0-0.ssl_100.ulinux.x64.rpm
+
+# Installing DSC:
+sudo wget -P /tmp/  https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_098.x64.rpm
+sudo rpm -Uvh /tmp/dsc-1.1.1-294.ssl_098.x64.rpm
