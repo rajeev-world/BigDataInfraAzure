b03ea19c3d72abeff3f638ba041609dc3701838f
rajeev-world <47442876+rajeev-world@users.noreply.github.com>
2019-03-08 16:28:46 +0530

    Update masterscript.sh

--- rajeev/sre/masterscript.sh
+++ rajeev/sre/masterscript.sh
@@ -5,12 +5,10 @@ sudo yum -y install python-pip
 sudo yum -y install python-devel
 sudo yum -y install gcc
 sudo pip install psutil
-
 wget -P ~/ https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/machinemetrics.py
 sudo chown -R babauser:babauser /usr/share/nginx/html
 wget -P /usr/share/nginx/html/ https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/track.txt
 wget -P ~/ https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/machinemetrics.py
 (crontab -l 2>/dev/null; echo "* * * * * python ~/machinemetrics.py  >> /usr/share/nginx/html/track.txt && sed -ni '3{h; :a; n; \${p;x;bb}; H; ba}; :b; p' /usr/share/nginx/html/track.txt ") | crontab -
-
-(crontab -l 2>/dev/null; echo "* * * * * wget -O machine1.txt -P /usr/share/nginx/html/  http://babamachine1/track.txt") | crontab -
-(crontab -l 2>/dev/null; echo "* * * * * wget -O machine2.txt -P /usr/share/nginx/html/  http://babamachine2/track.txt") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * wget -O /usr/share/nginx/html/machine1.txt   http://babamachine1/track.txt") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * wget -O /usr/share/nginx/html/machine2.txt   http://babamachine2/track.txt") | crontab -
