21ad1f1154e1af2229fdbedf6b6e72393d16685e
rajeev-world <47442876+rajeev-world@users.noreply.github.com>
2019-03-08 15:42:34 +0530

    Update myscript.sh

--- rajeev/sre/myscript.sh
+++ rajeev/sre/myscript.sh
@@ -8,6 +8,6 @@ sudo pip install psutil
 
 wget -P ~/ https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/machinemetrics.py
 sudo chown -R babauser:babauser /usr/share/nginx/html/
-https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/track.txt
-wget -P /usr/share/nginx/html/ https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/machinemetrics.py
+wget -P /usr/share/nginx/html/ https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/track.txt
+wget -P ~/ https://raw.githubusercontent.com/rajeev-world/BigDataInfraAzure/master/rajeev/sre/machinemetrics.py
 (crontab -l 2>/dev/null; echo "* * * * * python ~/machinemetrics.py  >> /usr/share/nginx/html/track.txt && sed -ni '3{h; :a; n; \${p;x;bb}; H; ba}; :b; p' /usr/share/nginx/html/track.txt ") | crontab -
