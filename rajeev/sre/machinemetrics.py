99c1ccc1e40ec41af9da60a104b944ac92a99535
rajeev-world <47442876+rajeev-world@users.noreply.github.com>
2019-03-14 11:08:50 +0530

    Update machinemetrics.py

--- rajeev/sre/machinemetrics.py
+++ rajeev/sre/machinemetrics.py
@@ -1,3 +1,3 @@
 import psutil
 import datetime
-print datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M UTC"),"       ", psutil.disk_usage('/').total,"        ", psutil.disk_usage('/').percent, "                ", psutil.virtual_memory().total,"            ",psutil.virtual_memory().percent,"                   ",psutil.cpu_percent(interval=10)
+print datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M UTC"),"       ", "{:,}".format(psutil.disk_usage('/').total/(1024*1024)),"MB           ", psutil.disk_usage('/').percent, "                ", "{:,}".format(psutil.virtual_memory().total/(1024*1024)),"MB               ",psutil.virtual_memory().percent,"                   ",psutil.cpu_percent(interval=10)
